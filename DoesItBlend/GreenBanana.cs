ca7cee050464c4e722af8dcf1f1224fa5e713b6d
danehoff <danehoff@ksu.edu>
2020-02-05 16:33:51 -0600

    Wednesday Week 3

---
+++ DoesItBlend/GreenBanana.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoesItBlend
+{
+    public class GreenBanana : Banana
+    {
+
+        public new string Blend()
+        {
+
+            return "lumps.";
+
+        }
+
+    }
+}
